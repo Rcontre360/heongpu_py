e74ee702f1214fa0ea41522d7d006755dad0477e
Rcontre360 <rcontreraspimentel@gmail.com>
2025-05-01 12:04:09 -0400

    feat: new ckks vector

--- src/heongpu/ckks_tensor.py
+++ src/heongpu/ckks_tensor.py
@@ -1,31 +1,58 @@
 from heongpu import _heongpu_api
 
-class CKKSTensor:
-    def __init__(self, context: _heongpu_api.Parameters, data: list[float], scale: float, public_key: _heongpu_api.Publickey):
-        self.context = context
+class CKKSContext:
+    parameters: _heongpu_api.Parameters
+    public_key: _heongpu_api.Publickey
+    private_key: _heongpu_api.SecretKey
+    relin_key: _heongpu_api.Relinkey
+    scale: float
+
+    def __init__(self, poly_modulus_degree:int, scale:int, coeff_mod_bit_sizes: list[int]):
+        _heongpu_api.set_device(0)
         self.scale = scale
-        self.encoder = _heongpu_api.HEEncoder(context)
-        self.encryptor = _heongpu_api.HEEncryptor(context, public_key)
-        self.operators = _heongpu_api.HEOperator(context)
+        self.parameters = _heongpu_api.Parameters(_heongpu_api.CKKS,_heongpu_api.METHOD_I)
+
+        self.parameters.set_poly_modulus_degree(poly_modulus_degree)
+        self.parameters.set_coeff_modulus(coeff_mod_bit_sizes,[ 60 ])
+
+        self.parameters.generate()
+
+    def generate_keys(self):
+        keygen = _heongpu_api.HEKeyGenerator(self.parameters)
+        self.private_key = _heongpu_api.SecretKey(self.parameters)
+        self.public_key = _heongpu_api.Publickey(self.parameters)
+        self.relin_key = _heongpu_api.Relinkey(self.parameters)
+
+        keygen.generate_secret_key(self.private_key)
+        keygen.generate_public_key(self.public_key, self.private_key)
+        keygen.generate_relin_key(self.relin_key, self.private_key)
+
+    def print_ckks_params(self):
+        self.parameters.print_parameters()
+
+class CKKSTensor:
+    context: CKKSContext
+
+    def __init__(self, context: CKKSContext, data:list[float]):
+        self.context = context
+        self.encoder = _heongpu_api.HEEncoder(self.context.parameters)
+        self.encryptor = _heongpu_api.HEEncryptor(self.context.parameters, context.public_key)
+        self.operators = _heongpu_api.HEOperator(self.context.parameters)
 
         # Store encoded plaintext
-        self.plain = _heongpu_api.Plaintext(context)
+        self.plain = _heongpu_api.Plaintext(self.context.parameters)
         vec = _heongpu_api.VecDouble(data)
-        self.encoder.encode(self.plain, vec, self.scale)
+        self.encoder.encode(self.plain, vec, self.context.scale)
 
         # Encrypt plaintext
-        self.ctxt = _heongpu_api.Ciphertext(context)
+        self.ctxt = _heongpu_api.Ciphertext(self.context.parameters)
         self.encryptor.encrypt(self.ctxt, self.plain)
 
-        self.relin_key = None
         self.exec_opts = _heongpu_api.ExecutionOptions()
 
-    def set_relin_key(self, relin_key: _heongpu_api.Relinkey):
-        self.relin_key = relin_key
-
-    def decrypt(self, secret_key: _heongpu_api.SecretKey) -> list[float]:
-        decryptor = _heongpu_api.HEDecryptor(self.context, secret_key)
-        self.plain = _heongpu_api.Plaintext(self.context)
+    def decrypt(self) -> list[float]:
+        decryptor = _heongpu_api.HEDecryptor(self.context.parameters, self.context.private_key)
+        self.plain = _heongpu_api.Plaintext(self.context.parameters)
         decryptor.decrypt(self.plain, self.ctxt)
 
         decoded = _heongpu_api.VecDouble([])
@@ -33,58 +60,59 @@ class CKKSTensor:
         return [decoded[i] for i in range(len(decoded))]
 
     def __add__(self, other: 'CKKSTensor') -> 'CKKSTensor':
-        result_ctxt = _heongpu_api.Ciphertext(self.context)
+        result_ctxt = _heongpu_api.Ciphertext(self.context.parameters)
         self.operators.add(self.ctxt, other.ctxt, result_ctxt, self.exec_opts)
-        return CKKSTensor.from_ciphertext(self.context, result_ctxt, self.scale, self.encoder, self.operators, self.relin_key)
+        return CKKSTensor.from_ciphertext(self.context, result_ctxt)
 
     def __iadd__(self, other: 'CKKSTensor'):
         self.operators.add_inplace(self.ctxt, other.ctxt, self.exec_opts)
         return self
 
     def __sub__(self, other: 'CKKSTensor') -> 'CKKSTensor':
-        result_ctxt = _heongpu_api.Ciphertext(self.context)
+        result_ctxt = _heongpu_api.Ciphertext(self.context.parameters)
         self.operators.sub(self.ctxt, other.ctxt, result_ctxt, self.exec_opts)
-        return CKKSTensor.from_ciphertext(self.context, result_ctxt, self.scale, self.encoder, self.operators, self.relin_key)
+        return CKKSTensor.from_ciphertext(self.context, result_ctxt)
 
     def __isub__(self, other: 'CKKSTensor'):
         self.operators.sub_inplace(self.ctxt, other.ctxt, self.exec_opts)
         return self
 
     def __mul__(self, other: 'CKKSTensor') -> 'CKKSTensor':
-        result_ctxt = _heongpu_api.Ciphertext(self.context)
+        result_ctxt = _heongpu_api.Ciphertext(self.context.parameters)
         self.operators.multiply(self.ctxt, other.ctxt, result_ctxt, self.exec_opts)
-        if self.relin_key:
-            self.operators.relinearize_inplace(result_ctxt, self.relin_key, self.exec_opts)
-        return CKKSTensor.from_ciphertext(self.context, result_ctxt, self.scale, self.encoder, self.operators, self.relin_key)
+        self.operators.relinearize_inplace(result_ctxt, self.context.relin_key, self.exec_opts)
+        return CKKSTensor.from_ciphertext(self.context, result_ctxt)
 
     def __imul__(self, other: 'CKKSTensor'):
         self.operators.multiply_inplace(self.ctxt, other.ctxt, self.exec_opts)
-        if self.relin_key:
-            self.operators.relinearize_inplace(self.ctxt, self.relin_key, self.exec_opts)
+        self.operators.relinearize_inplace(self.ctxt, self.context.relin_key, self.exec_opts)
         return self
 
     def __neg__(self) -> 'CKKSTensor':
-        result_ctxt = _heongpu_api.Ciphertext(self.context)
+        result_ctxt = _heongpu_api.Ciphertext(self.context.parameters)
         self.operators.negate(self.ctxt, result_ctxt, self.exec_opts)
-        return CKKSTensor.from_ciphertext(self.context, result_ctxt, self.scale, self.encoder, self.operators, self.relin_key)
+        return CKKSTensor.from_ciphertext(self.context, result_ctxt)
 
     def __str__(self):
         return "<CKKSTensor (encrypted)>"
 
-    def decrypted_str(self, secret_key: _heongpu_api.SecretKey, max_items: int = 10) -> str:
-        values = self.decrypt(secret_key)
+    def decrypted_str(self,  max_items: int = 10) -> str:
+        values = self.decrypt(self.context.private_key)
         return f"<CKKSTensor {values[:max_items]}...>"
 
     @classmethod
-    def from_ciphertext(cls, context, ctxt, scale, encoder, operators, relin_key):
+    def from_ciphertext(cls, context:CKKSContext, ctxt:_heongpu_api.Ciphertext):
         obj = cls.__new__(cls)
         obj.context = context
-        obj.scale = scale
-        obj.encoder = encoder
-        obj.operators = operators
+        obj.encoder = _heongpu_api.HEEncoder(context.parameters)
+        obj.encryptor = _heongpu_api.HEEncryptor(context.parameters, context.public_key)
+        obj.operators = _heongpu_api.HEOperator(context.parameters)
+        obj.exec_opts = _heongpu_api.ExecutionOptions()
+
         obj.ctxt = ctxt
-        obj.relin_key = relin_key
         obj.exec_opts = _heongpu_api.ExecutionOptions()
         obj.plain = None  # will be set on decryption
+
         return obj
+
 
--- test.py
+++ test.py
@@ -1,35 +1,24 @@
-from heongpu import  HEKeyGenerator, set_device, CKKSTensor,Publickey, SecretKey, Parameters, SCHEMES, KEY_SWITCHING_TYPES, SECURITY_LEVELS
+from heongpu import  HEKeyGenerator, set_device, Publickey, SecretKey, Parameters, SCHEMES, KEY_SWITCHING_TYPES, SECURITY_LEVELS
 from heongpu import _heongpu_api
-import numpy as np
+from ckks import CKKSContext, CKKSTensor
 
-set_device(0)
-context = Parameters(_heongpu_api.CKKS, _heongpu_api.METHOD_I)
 poly_modulus_degree = 32768
 coeff_mod_bit_sizes = [60, 40, 40, 60]
 scale = 2**40
 vec_size = 4096
 repetitions = 100
 
-context.set_poly_modulus_degree(poly_modulus_degree)
-context.set_coeff_modulus(coeff_mod_bit_sizes,[ 60 ])
+context = CKKSContext(poly_modulus_degree, scale, coeff_mod_bit_sizes)
+context.generate_keys()
+context.print_ckks_params()
 
-context.generate()
-context.print_parameters()
+message = [i for i in range(100)]
+t1 = CKKSTensor(context, message)
 
-keygen = HEKeyGenerator(context)
-secret_key = SecretKey(context)
-public_key = Publickey(context)
+message2 = [i for i in range(100)]
+t2 = CKKSTensor(context, message2)
 
-keygen.generate_secret_key(secret_key)
-keygen.generate_public_key(public_key, secret_key)
+t2 -= t1
 
-message = [i + 10 for i in range(100)]
-t1 = CKKSTensor(context, message, scale, public_key)
-
-message2 = [i + 20 for i in range(100)]
-t2 = CKKSTensor(context, message2, scale, public_key)
-
-t3 = t1+t2
-
-print(t3.decrypt(secret_key)[0:10])
+print(t2.decrypt()[0:10])
 
